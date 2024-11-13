f0f15277c809cbae2a26ea53f81bc083f4795d5e
chavi-bernstein <101974431+chavi-bernstein@users.noreply.github.com>
2024-11-13 22:15:59 +0200

    Connect PostgreSQL to the project.

--- server.ts
+++ server.ts
@@ -1,15 +1,19 @@
 import dotenv from 'dotenv';
 import express, { Express, Request, Response , Application } from 'express';
 import cors from "cors";
-import corsOptions from './config/corsOptions';
+// import corsOptions from './config/corsOptions';
 import garageRouter from './routes/garage';
+import { connectDB } from './config/dbConn'; 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(corsOptions));
+connectDB()
+
+app.use(cors());
+
 app.use('/garages', garageRouter);
 
 app.listen(PORT, () => {
