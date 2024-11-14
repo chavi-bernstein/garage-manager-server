4097e9f44754555349b47c0ab40e5bd8173b1011
chavi-bernstein <101974431+chavi-bernstein@users.noreply.github.com>
2024-11-14 22:40:10 +0200

    Remove unnecessary required.

--- models/garage.ts
+++ models/garage.ts
@@ -19,17 +19,17 @@ interface IGarage extends Document {
 const garageSchema: Schema = new Schema({
     mispar_mosah: { type: Number, required: true },
     shem_mosah: { type: String, required: true },
-    cod_sug_mosah: { type: Number, required: true },
-    sug_mosah: { type: String, required: true },
-    ktovet: { type: String, required: true },
-    yishuv: { type: String, required: true },
-    telephone: { type: String, required: true },
-    mikud: { type: Number, required: true },
-    cod_miktzoa: { type: Number, required: true },
-    miktzoa: { type: String, required: true },
-    menahel_miktzoa: { type: String, required: true },
-    rasham_havarot: { type: Number, required: true },
-    TESTIME: { type: String, default: "" }
+    cod_sug_mosah: { type: Number },
+    sug_mosah: { type: String },
+    ktovet: { type: String },
+    yishuv: { type: String },
+    telephone: { type: String },
+    mikud: { type: Number },
+    cod_miktzoa: { type: Number },
+    miktzoa: { type: String },
+    menahel_miktzoa: { type: String },
+    rasham_havarot: { type: Number },
+    TESTIME: { type: String }
 });
 
 const Garage = mongoose.model<IGarage>("Garage", garageSchema);
